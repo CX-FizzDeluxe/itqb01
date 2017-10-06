f62ac96fe6b3f8215f049625d41d7cda1bc48da4
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-10-06 13:58:46 +0300

    Fri Oct 06 13:58:46 EEST 2017

--- src/main/java/befaster/solutions/FizzBuzz.java
+++ src/main/java/befaster/solutions/FizzBuzz.java
@@ -1,11 +1,19 @@
 package befaster.solutions;
 
-import befaster.runner.SolutionNotImplementedException;
-
 public class FizzBuzz {
 
     public static String fizzBuzz(Integer number) {
-        throw new SolutionNotImplementedException();
+        boolean multipleOf3 = (number % 3) == 0;
+        boolean multipleOf5 = (number % 5) == 0;
+        if (multipleOf3 && multipleOf5) {
+            return "fizz buzz";
+        } else if (multipleOf3) {
+            return "fizz";
+        } else if (multipleOf5) {
+            return "buzz";
+        } else {
+            return number.toString();
+        }
     }
 
 }
