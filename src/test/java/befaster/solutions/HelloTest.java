17d8698c384aa68820f29507088e260d989a9611
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-10-06 13:48:16 +0300

    Fri Oct 06 13:48:16 EEST 2017

--- src/test/java/befaster/solutions/HelloTest.java
+++ src/test/java/befaster/solutions/HelloTest.java
@@ -7,6 +7,6 @@ public class HelloTest {
 
     @Test
     public void test_hello_message() {
-        Assert.assertEquals(Hello.hello("Mr. X"), "Hello Mr. X");
+        Assert.assertEquals(Hello.hello("Mr. X"), "Hello, World!");
     }
 }
