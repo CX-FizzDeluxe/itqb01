04a266308e6d05973f492fc7842d6088b02265e7
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-10-06 14:10:52 +0300

    Fri Oct 06 14:10:52 EEST 2017

---
+++ src/test/java/befaster/solutions/FizzBuzzR3Test.java
@@ -0,0 +1,34 @@
+package befaster.solutions;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FizzBuzzR2Test {
+
+    @Test
+    public void test_is_fizz() {
+        Assert.assertEquals("fizz", FizzBuzz.fizzBuzz(3));
+        Assert.assertEquals("fizz", FizzBuzz.fizzBuzz(21));
+        Assert.assertEquals("fizz", FizzBuzz.fizzBuzz(31));
+    }
+
+    @Test
+    public void test_is_buzz() {
+        Assert.assertEquals("buzz", FizzBuzz.fizzBuzz(5));
+        Assert.assertEquals("buzz", FizzBuzz.fizzBuzz(25));
+        Assert.assertEquals("buzz", FizzBuzz.fizzBuzz(52));
+    }
+
+    @Test
+    public void test_fizz_and_buzz() {
+        Assert.assertEquals("fizz buzz", FizzBuzz.fizzBuzz(0));
+        Assert.assertEquals("fizz buzz", FizzBuzz.fizzBuzz(15));
+        Assert.assertEquals("fizz buzz", FizzBuzz.fizzBuzz(53));
+    }
+
+    @Test
+    public void test_neither_fizz_nor_buzz() {
+        Assert.assertEquals("1", FizzBuzz.fizzBuzz(1));
+        Assert.assertEquals("4", FizzBuzz.fizzBuzz(4));
+    }
+}
