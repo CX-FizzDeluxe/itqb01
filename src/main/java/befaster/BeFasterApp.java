158efe8fad05a5121aef0e7c37e4bc51699b37e9
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-10-06 14:21:32 +0300

    Fri Oct 06 14:21:32 EEST 2017

--- src/main/java/befaster/BeFasterApp.java
+++ src/main/java/befaster/BeFasterApp.java
@@ -48,7 +48,7 @@ public class BeFasterApp {
     public static void main(String[] args) throws ConfigNotFoundException {
         ClientRunner.forUsername(readFromConfigFile("tdl_username"))
                 .withServerHostname("run.befaster.io")
-                .withActionIfNoArgs(RunnerAction.deployToProduction)
+                .withActionIfNoArgs(RunnerAction.getNewRoundDescription)
                 .start(args);
     }
 
